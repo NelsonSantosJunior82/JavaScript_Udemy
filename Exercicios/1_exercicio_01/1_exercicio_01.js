8ca4ade5d8c8d091a58211202d649164bcb82f24
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-09-24 19:20:56 -0300

    criei scrirpt  de exercício 01

---
+++ Exercicios/1_exercicio_01/1_exercicio_01.js
@@ -0,0 +1,4 @@
+console.log("Isso é uma string com aspas duplas");
+console.log('Isso é uma string com aspas simples');
+console.log(`Isso é uma string literal com 'aspas simples'`);
+console.log(`Isso é uma string literal com "aspas duplas"`);
