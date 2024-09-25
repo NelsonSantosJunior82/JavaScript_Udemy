dc6f2faafb6532c7305da6f2a105b510443334f2
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-09-25 19:48:48 -0300

    Criei script de variaveis

---
+++ 03_Estrutura_de_programacao/1_variaveis/variaveis.js
@@ -0,0 +1,24 @@
+let nome = "Nelson" ;
+
+console.log(nome);
+console.log(`Meu nome é ${nome}.`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "laranjas";
+
+console.log(nome);
+
+laranjas = 83284;
+
+console.log(laranjas);
+
+laranjas = "laranjas";
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
