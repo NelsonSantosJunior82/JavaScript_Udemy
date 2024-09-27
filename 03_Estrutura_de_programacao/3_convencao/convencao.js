0842aa8b9e39202b56a915cca90ab5d5598e021b
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-09-26 21:40:47 -0300

    Criei convencao.js

---
+++ 03_Estrutura_de_programacao/3_convencao/convencao.js
@@ -0,0 +1,10 @@
+// let 3nome = 'teste'; -- sintax inválida
+// let @nome = 'teste1'; -- sintax inválida
+// let !nome = 'teste2'; -- sintax inválida
+let nome3 = 'teste3';
+let $nome = 'teste4';
+let _nome = 'teste5';
+let Nome = 'teste6';
+let seuNome = 'teste7';
+let seu_nome = 'teste';
+
