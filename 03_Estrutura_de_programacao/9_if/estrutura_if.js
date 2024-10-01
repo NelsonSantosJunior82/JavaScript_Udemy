b3914636b623affb82d26ddb66053e626c73d674
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-01 18:13:18 -0300

    Criei script if

---
+++ 03_Estrutura_de_programacao/9_if/estrutura_if.js
@@ -0,0 +1,18 @@
+let idade = 19;
+
+
+if(idade == 19){
+    console.log('Pode Entrar!');
+}
+
+if(idade > 25){
+    console.log('Idade maior que 25!');
+}
+
+
+let nome = "Nelson";
+
+if(nome == "Nelson" && idade > 10){
+    console.log("Liberado!");
+}
+
