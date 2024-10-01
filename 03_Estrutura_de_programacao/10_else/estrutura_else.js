38a72cf4d137c1bc8733587ef3dc190bf0dacea3
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-01 18:43:30 -0300

    Criei script estrutura else

---
+++ 03_Estrutura_de_programacao/10_else/estrutura_else.js
@@ -0,0 +1,7 @@
+let velocidade = 100;
+
+if(velocidade <= 80){
+    console.log('Você está abaixo do limite de velocidade!');
+} else{
+    console.log('Você foi multado por ultrapassar o limite de velocidade!')
+}
