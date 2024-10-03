e99a683816b730a20416b2daf21357d49e4bc637
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-03 18:37:50 -0300

    Criei script de estrutura switch

---
+++ 03_Estrutura_de_programacao/19_switch/switch.js
@@ -0,0 +1,13 @@
+let nome = "Nelson";
+
+switch(nome){
+    case "Nelson":
+        console.log("O nome é Nelson");
+        break;
+    case "Janaina":
+        console.log("O nome é Janaina");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+}
