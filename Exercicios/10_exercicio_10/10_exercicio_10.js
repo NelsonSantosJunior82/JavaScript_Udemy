fb23ec8ee3e31ace06ccc5b5a3e7575d8696e2ba
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-04 21:52:15 -0300

    Criei script de exercicio 10

---
+++ Exercicios/10_exercicio_10/10_exercicio_10.js
@@ -0,0 +1,11 @@
+velocidade = 80;
+
+if(velocidade <= 100){
+
+    console.log("Você está dentro do limite de velocidade, continue assim!!");
+    
+} else {
+
+    console.log("Você está acima da velocidade e será Multado!");
+
+}
