35def8db988f9c9f5f453a8f338223cc12911c29
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-04 21:54:20 -0300

    Criei script de exercicio 11

---
+++ Exercicios/11_exercicio_11/11_exercicio_11.js
@@ -0,0 +1,9 @@
+let idade = 19;
+let cnh = true;
+
+if(idade >= 18 && cnh == false){
+    console.log("Você não está apto a dirigir!");
+} else {
+    console.log("Você está apto a dirigir!")
+    
+}
