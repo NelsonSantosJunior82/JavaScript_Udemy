e3f84e023ca2e2ded9e27cfcf3f517be19dc0b30
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-08 20:08:37 -0300

    Criei index de definindo funções

---
+++ 04_Funcoes/1_definindo_funcoes/definindo_funcao.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole(){
+    console.log("Olá Mundo!");
+}
+
+imprimirNoConsole();
+
+function imprimirNumero(num){
+    console.log(" o número é: " + num);
+}
+
+imprimirNumero(9);
+imprimirNumero(8);
+imprimirNumero(7);
+imprimirNumero(6);
+
+const numeroAleatorio = function(){
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
