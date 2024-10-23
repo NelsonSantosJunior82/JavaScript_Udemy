c4e15b7509144a9cea86ed5f14f4c31ce169533d
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-10-23 17:23:20 -0300

    Criei script de exercício 20

---
+++ Exercicios/20_exercicio_20/20_exercicio_20.js
@@ -0,0 +1,10 @@
+function podeEntrar(idade){
+    if (idade >= 18){
+        console.log("Pode entrar na Auto Escola")
+    }else{
+        console.log("Não pode entrar na Auto Escola!")
+    }
+}
+
+podeEntrar(19);
+podeEntrar(15);
