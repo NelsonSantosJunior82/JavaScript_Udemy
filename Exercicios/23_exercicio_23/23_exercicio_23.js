a3421e950011cc89b9b7dcc6a30e383060c7a109
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-11-20 09:28:25 -0300

    script de exercício 23

---
+++ Exercicios/23_exercicio_23/23_exercicio_23.js
@@ -0,0 +1,11 @@
+function checarTamanhoTexto(texto){
+    if(texto.length > 10){
+        console.log("Texto muito longo!");
+    } else {
+        console.log("Texto dentro do limite!")
+    }
+
+}
+
+checarTamanhoTexto("Nelson")
+checarTamanhoTexto("Janaina Nunes")
