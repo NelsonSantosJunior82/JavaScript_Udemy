fc9676b69828295cba3a17e731f8ddcce2de014a
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-12-30 19:46:36 -0300

    Criei script de keys

---
+++ 05_Arrays/8_Keys/8_keys.js
@@ -0,0 +1,9 @@
+let obj ={
+    'chave 1': 1, //'chave 1:' é a chave do objeto e em seguida o valor da chave
+    'chave 2': 2,
+    'chave 3': 3,
+};
+
+console.log(obj);
+
+console.log(Object.keys(obj));
