a498f187c71676770a14bf46c5603c36a333c284
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-02 22:06:09 -0300

    Criei script de shift unshift

---
+++ 05_Arrays/12_Shift_Unshift/12_shift_unshift.js
@@ -0,0 +1,11 @@
+let nomes = ["Nelson","Janaina","Yasmin","Pietro","Maria","Ravi"];
+
+let removerPrimeiros = nomes.shift();
+
+console.log(removerPrimeiros);
+console.log(nomes);
+
+nomes.unshift("Nelson")
+
+console.log(nomes);
+console.log(nomes[0]);
