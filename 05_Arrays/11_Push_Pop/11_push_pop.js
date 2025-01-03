ccd25dcdca2d2a539cedf27121c29e6425d5a823
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-02 22:05:06 -0300

    Criei script de push pop

---
+++ 05_Arrays/11_Push_Pop/11_push_pop.js
@@ -0,0 +1,10 @@
+let nomes = ["Nelson","Janaina","Yasmin","Pietro","Maria","Ravi"];
+
+let elementosRemovidos = nomes.pop(); // Método pop retira o último item de uma array
+
+console.log(elementosRemovidos);
+console.log(nomes);
+
+nomes.push("Ravi","Eloah"); //Método push adiciona um último item na array
+
+console.log(nomes);
