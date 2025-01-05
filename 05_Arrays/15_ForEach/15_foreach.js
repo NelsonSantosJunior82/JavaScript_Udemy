9ca034af928ddda18651f66199fda7bd55587399
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-04 21:40:03 -0300

    Criei script de ForEach

---
+++ 05_Arrays/15_ForEach/15_foreach.js
@@ -0,0 +1,10 @@
+let nomes = ["Nelson","Janaina","Yasmin","Pietro","Maria","Ravi"]
+let nums = [0,1,2,3,4,5,6,7,8,9]
+
+nomes.forEach(nome => {
+    console.log("O Nome é "+ nome);
+});// nome[i]
+
+nums.forEach(num => {
+    console.log("O número é: "+ num);
+});//num[i]
