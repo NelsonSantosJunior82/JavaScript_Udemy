b6f297662ab63ae8ccb73816c9333be1c92151e0
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-07 09:21:43 -0300

    Criei script split em arrays

---
+++ 05_Arrays/20_Split/20_split.js
@@ -0,0 +1,11 @@
+let frase = "Eu sou um excelente programador";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let frutas = "Maça;Banana;Abacaxi;Goiaba;Manga";
+
+console.log(frutas.split(";"));
