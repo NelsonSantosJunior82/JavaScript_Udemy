cdd62cb39a8f825f1ce5772e4c7c68657a045152
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-07 09:19:31 -0300

    Criei script de trim em arrays

---
+++ 05_Arrays/18_Trim/18_trim.js
@@ -0,0 +1,12 @@
+let nome = "    Nelson";
+let nomeEspacado = "  \n Nelson \n   ";
+
+let nomeCorrigido = nome.trim();
+let nomeEspCorrigido = nomeEspacado.trim();
+
+
+console.log(nome);
+console.log(nomeCorrigido);
+console.log(nomeEspacado);
+console.log(nomeEspCorrigido);
+
