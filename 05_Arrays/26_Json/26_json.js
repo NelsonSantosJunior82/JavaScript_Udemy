f3098c1b16201e503a0f426d09e6c5a46057f544
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-09 16:30:40 -0300

    Criei script de Json

---
+++ 05_Arrays/26_Json/26_json.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    "Nome": "Nelson",
+    "Idade": 42,
+    "Conjuge": "Janaina",
+    "Filhos":["Pietro","Maria Alice","Ravi"]
+}
+
+console.log(pessoa.Nome)
+console.log(pessoa.Idade)
+console.log(pessoa.Conjuge)
+console.log(pessoa.Filhos)
