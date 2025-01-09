8d26b93a2712410caa0aab414a39a0e8f913881a
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-09 16:27:10 -0300

    Criei script de destructuring objetos em arrays

---
+++ 05_Arrays/24_Destructuring/24_destructuring.js
@@ -0,0 +1,13 @@
+let carro = {
+    rodas: 4,
+    portas: 5,
+    tetosolar: true,
+    motor:"2.0"
+}
+
+const {rodas: rd, portas: pt, tetosolar: ts, motor: mt} = carro;
+
+console.log(rd)
+console.log(pt)
+console.log(ts)
+console.log(mt)
