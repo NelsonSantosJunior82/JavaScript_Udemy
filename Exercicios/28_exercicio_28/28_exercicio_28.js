289b5401e01649a51fdd167dd382897e03902afc
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-09 22:11:17 -0300

    Criei script de ex28

---
+++ Exercicios/28_exercicio_28/28_exercicio_28.js
@@ -0,0 +1,13 @@
+let onibus = {
+    rodas: 8,
+    limite: 40,
+    portas: 2
+}
+
+console.log(onibus.rodas);
+console.log(onibus.limite);
+console.log(onibus.portas);
+
+//objeto = onibus
+//propriedades = rodas,limite e portas
+//8,40,2 = valor da propriedade
