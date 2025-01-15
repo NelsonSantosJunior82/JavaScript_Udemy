fb8bcd52307570e1843a55aee0151104d5f0453b
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-15 16:50:53 -0300

    Criei script de ex030

---
+++ Exercicios/30_exercicio_30/30_exercicio_30.js
@@ -0,0 +1,7 @@
+nomes = ["Janaina","Pietro","Maria","Nelson","Ravi","Yasmin"]
+
+if (nomes.includes("Roberto")){
+    console.log("Nome encontrado!")
+}else{
+    console.log("Nome não encontrado!")
+}
