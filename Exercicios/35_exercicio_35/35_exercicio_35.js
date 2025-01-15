d63a05095469ac015d5574eb2c7d5a0605ac1470
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-15 17:00:51 -0300

    Criei script de ex035

---
+++ Exercicios/35_exercicio_35/35_exercicio_35.js
@@ -0,0 +1,22 @@
+/*   const calculadora = {
+        soma: function(a,b){
+            return a + b;
+            },
+        subtrair: function(a,b){
+            return a - b;
+            },
+        mutiplicar: function(a,b){
+            return a * b;
+            },
+        dividir: function(a,b){
+            return a / b;
+            },
+
+    }
+
+    console.log(calculadora.soma(3,4));
+    console.log(calculadora.subtrair(8,4));
+    console.log(calculadora.mutiplicar(8,6));
+    console.log(calculadora.dividir(10,2));
+ */
+
