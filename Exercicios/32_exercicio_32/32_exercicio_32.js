976df2415566ffaf2c043fb92e1029bc89c94f2e
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-15 16:57:20 -0300

    Criei script de ex032

---
+++ Exercicios/32_exercicio_32/32_exercicio_32.js
@@ -0,0 +1,6 @@
+let nomes = ["Janaina","Pietro","Maria","Nelson","Ravi","Yasmin"]
+
+for(let i = 0; i < nomes.length; i++){
+    console.log(nomes[i]);
+}
+//indice da array começa com "0"; enquanto indice da array for menor que o tamanho da array; indice recebe ("=") indice + 1
