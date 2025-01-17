cd2f6838351610d47c3a57f7eef6ff082c260d0d
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-17 17:51:51 -0300

    Criei script de classes02

---
+++ 06_Metodos/classes_02/classes.js
@@ -0,0 +1,16 @@
+function criarCachoroo(raca, patas, cor){
+    let cachorro = Object.create({});
+    cachorro.raca = raca;
+    cachorro.patas = patas;
+    cachorro.cor = cor;
+    cachorro.latir = function(){
+        console.log("Au, au");
+    }
+    return cachorro;
+}
+
+let doberman = criarCachoroo('Doberman', 4 , 'preto');
+
+console.log(doberman);
+
+doberman.latir();
