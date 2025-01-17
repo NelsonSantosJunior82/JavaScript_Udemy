d4cb29cd0bba12c0d9d8a91dae87716a9646c5bf
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-01-17 17:43:37 -0300

    Criei script de métodos01

---
+++ 06_Metodos/metodos_01/metodos.js
@@ -0,0 +1,12 @@
+const cachorro = {
+    uivar: function(){
+        console.log("AAAuuuuuu");
+    },
+
+    rosnar: function(){
+        console.log("GGGrrrrrr");
+    }
+}
+
+cachorro.uivar()
+cachorro.rosnar()
