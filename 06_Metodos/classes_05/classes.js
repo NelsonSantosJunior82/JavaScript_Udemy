3b12812c1988f20e9a2b3f300fb125e26966db35
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2025-06-02 20:19:10 -0300

    Criação de script em classes 05

---
+++ 06_Metodos/classes_05/classes.js
@@ -0,0 +1,13 @@
+//Classe e Construtor
+
+class Cachorro {
+    constructor(raca, patas, cor){
+        this.raca = raca;
+        this.patas = patas;
+        this.cor = cor;
+    }
+}
+
+let labrador = new Cachorro("Labrador", 4, "Amarelo");
+
+console.log(labrador);
